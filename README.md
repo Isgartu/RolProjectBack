bdec2abdac8e53c33d94a1fb0dd77c1c6ba28ee3
israel.garzon <israel.garzon@am-mad-por-560.atmira.global>
2019-01-11 14:04:10 +0100

    Added more data to README.md and data.sql

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # RolProjectBack
-Project based in Pathfinder Rol Game
+
+## Project based in Pathfinder Rol Game
+
+Info: https://es.wikipedia.org/wiki/Pathfinder_(juego_de_rol)
+
+Info: http://devir.es/producto/pathfinder-reglas-basicas/
+
+## Setup the Application
+
+1. Input data in data.sql / data-h2.sql
+
+2. Type `mvn spring-boot:run` from the root directory of the project to run the application. Or automatic run with your favourite IDE
+
+## Endpoints
+
+### Players
+
+localhost:8080/pathfinder/v1/players
+Shows all global players information
+
+localhost:8080/pathfinder/v1/players/1
+Show details player information
+
+localhost:8080/pathfinder/v1/players/search?charactername=charactername1
+Search player information by charactername
+
+localhost:8080/pathfinder/v1/players/search?gamername=gamername2
+Search player information by gamername
+
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,22 +2,30 @@ INSERT INTO `test`.`body` (`id`, `armor`, `arms`, `feet`, `gloves`, `head`, `leg
 INSERT INTO `test`.`body` (`id`, `armor`, `arms`, `feet`, `gloves`, `head`, `legs`, `neck`, `shoulders`) VALUES ('2', 'armor2', 'arms2', 'feet2', 'gloves2', 'head2', 'legs2', 'neck2', 'shoulders2');
 
 INSERT INTO `test`.`clothing` (`id`, `body_id`) VALUES ('1', '1');
+INSERT INTO `test`.`clothing` (`id`, `body_id`) VALUES ('2', '2');
 
-INSERT INTO `test`.`item` (`id`, `category`, `description`, `name`, `price`, `weight`) VALUES ('1', 'category1', 'description1', 'name1', '10', '10');
-INSERT INTO `test`.`item` (`id`, `category`, `description`, `name`, `price`, `weight`) VALUES ('2', 'category2', 'description2', 'name2', '20', '20');
+INSERT INTO `test`.`item` (`id`, `category`, `description`, `name`, `price`, `weight`) VALUES ('1', 'category1', 'description1', 'name1', '11', '11');
+INSERT INTO `test`.`item` (`id`, `category`, `description`, `name`, `price`, `weight`) VALUES ('2', 'category2', 'description2', 'name2', '22', '22');
+INSERT INTO `test`.`item` (`id`, `category`, `description`, `name`, `price`, `weight`) VALUES ('3', 'category3', 'description3', 'name3', '33', '33');
+INSERT INTO `test`.`item` (`id`, `category`, `description`, `name`, `price`, `weight`) VALUES ('4', 'category4', 'description4', 'name4', '44', '44');
 
-INSERT INTO `test`.`player` (`id`, `charactername`, `gamerclass`, `gamername`, `level`, `vitality`, `clothing_id`) VALUES ('1', 'charactername1', 'gamerclass1', 'gamername1', '10', '200', '1');
+INSERT INTO `test`.`qualities` (`id`, `charisma`, `intelligence`, `physique`, `power`, `skill`, `wisdom`) VALUES ('1', '11', '11', '11', '11', '11', '11');
+INSERT INTO `test`.`qualities` (`id`, `charisma`, `intelligence`, `physique`, `power`, `skill`, `wisdom`) VALUES ('2', '22', '22', '22', '22', '22', '22');
 
+INSERT INTO `test`.`weapon` (`id`, `bonus`, `critical`, `damage`, `munition`, `name`, `otherinfo`, `scope`, `type`) VALUES ('1', '11', '11', '11', '11', '11', 'other info1', '11', '11');
+INSERT INTO `test`.`weapon` (`id`, `bonus`, `critical`, `damage`, `munition`, `name`, `otherinfo`, `scope`, `type`) VALUES ('2', '12', '12', '12', '12', '12', 'other info2', '12', '12');
+INSERT INTO `test`.`weapon` (`id`, `bonus`, `critical`, `damage`, `munition`, `name`, `otherinfo`, `scope`, `type`) VALUES ('3', '21', '21', '21', '21', '21', 'other info3', '21', '21');
+INSERT INTO `test`.`weapon` (`id`, `bonus`, `critical`, `damage`, `munition`, `name`, `otherinfo`, `scope`, `type`) VALUES ('4', '22', '22', '22', '22', '22', 'other info4', '22', '22');
 
-INSERT INTO `test`.`qualities` (`id`, `charisma`, `intelligence`, `physique`, `power`, `skill`, `wisdom`) VALUES ('1', '21', '23', '56', '52', '21', '89');
-INSERT INTO `test`.`qualities` (`id`, `charisma`, `intelligence`, `physique`, `power`, `skill`, `wisdom`) VALUES ('2', '56', '54', '89', '52', '20', '45');
-
-INSERT INTO `test`.`weapon` (`id`, `bonus`, `critical`, `damage`, `munition`, `name`, `otherinfo`, `scope`, `type`) VALUES ('1', '20', '30', '45', '58', '52', 'other info', '26', '65');
-INSERT INTO `test`.`weapon` (`id`, `bonus`, `critical`, `damage`, `munition`, `name`, `otherinfo`, `scope`, `type`) VALUES ('2', '30', '50', '66', '88', '52', 'other info2', '22', '22');
+INSERT INTO `test`.`player` (`id`, `charactername`, `gamerclass`, `gamername`, `level`, `vitality`, `clothing_id`, `qualities_id`) VALUES ('1', 'charactername1', 'gamerclass1', 'gamername1', '11', '111', '1', '1');
+INSERT INTO `test`.`player` (`id`, `charactername`, `gamerclass`, `gamername`, `level`, `vitality`, `clothing_id`, `qualities_id`) VALUES ('2', 'charactername2', 'gamerclass2', 'gamername2', '22', '222', '2', '2');
 
 INSERT INTO `test`.`player_weapon` (`player_id`, `weapon_id`) VALUES ('1', '1');
 INSERT INTO `test`.`player_weapon` (`player_id`, `weapon_id`) VALUES ('1', '2');
+INSERT INTO `test`.`player_weapon` (`player_id`, `weapon_id`) VALUES ('2', '3');
+INSERT INTO `test`.`player_weapon` (`player_id`, `weapon_id`) VALUES ('2', '4');
 
 INSERT INTO `test`.`player_inventory` (`player_id`, `inventory_id`) VALUES ('1', '1');
 INSERT INTO `test`.`player_inventory` (`player_id`, `inventory_id`) VALUES ('1', '2');
-
+INSERT INTO `test`.`player_inventory` (`player_id`, `inventory_id`) VALUES ('2', '3');
+INSERT INTO `test`.`player_inventory` (`player_id`, `inventory_id`) VALUES ('2', '4');
