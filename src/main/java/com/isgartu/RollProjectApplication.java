c4b161fd50f4b789f5c6c1847c2edf007bb6b520
israel.garzon <israel.garzon@am-mad-por-560.atmira.global>
2019-01-11 13:53:51 +0100

    Added modelmapper dependency & bean

--- src/main/java/com/isgartu/RollProjectApplication.java
+++ src/main/java/com/isgartu/RollProjectApplication.java
@@ -1,12 +1,19 @@
 package com.isgartu;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 
 @EntityScan(basePackages="com.isgartu")
 @SpringBootApplication
 public class RollProjectApplication {
+	
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(RollProjectApplication.class, args);
